7dd9bbd45ac12e5458834f159c837a4117828acd
gloriacassell <g239111@gmail.com>
2016-02-19 16:15:48 -0800

    adding file

---
+++ JavaScript/Multiple Identical Options in Switch Statements.js
@@ -0,0 +1,52 @@
+// JavaScript source code
+/*Multiple Identical Options in Switch Statements*/
+
+function myTest(val) {
+    var answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+            result = "1";
+            return "Low";
+            break;
+        case 2:
+            result = "2";
+            return "Low";
+            break;
+        case 3:
+            result = "3";
+            return "Low";
+            break;
+        case 4:
+            result = "4";
+            return "Mid";
+            break;
+        case 5:
+            result = "5";
+            return "Mid";
+            break;
+        case 6:
+            result = "6";
+            return "Mid";
+            break;
+        case 7:
+            result = "7"
+            return "High";
+            break;
+        case 8:
+            result = "8"
+            return "High";
+            break;
+        case 9:
+            result = "9"
+            return "High";
+            break;
+    }
+
+
+    // Only change code above this line  
+    return answer;
+}
+
+// Change this value to test
+myTest(1);
