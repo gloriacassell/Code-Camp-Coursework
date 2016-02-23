b432b75df4f3def70e97eb0a607ba2150f1fa59d
gloriacassell <g239111@gmail.com>
2016-02-22 21:18:20 -0800

    adding file

---
+++ JavaScript/Adding a default option in Switch statements.js
@@ -0,0 +1,25 @@
+// JavaScript source code
+
+function myTest(val) {
+    var answer = "";
+   
+    switch (val) {
+        case "a":
+            return "apple";
+            break;
+        case "b":
+            return "bird";
+            break;
+        case "c":
+            return "cat";
+            break;
+        default:
+            return "stuff";
+    }
+
+
+    return answer;
+}
+
+
+myTest(1);
