f768c967edaf577dc47ecc96728ed1a9c4cc411a
gloriacassell <g239111@gmail.com>
2016-02-22 21:23:22 -0800

    adding file

---
+++ JavaScript/Selecting from many options with Switch Statements.js
@@ -0,0 +1,28 @@
+// JavaScript source code
+
+function myTest(val) {
+    var answer = "";
+
+    switch (val) {
+        case 1:
+            return "alpha";
+            break;
+        case 2:
+            return "beta";
+        case 3:
+            return "gamma";
+            break;
+        case 4:
+            return "delta";
+            break;
+    }
+
+
+
+
+    return answer;
+}
+
+
+myTest(1);
+
